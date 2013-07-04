7a75152e0fdb3ff5d0b93b2e861bad0ab546d055
Ashish Khedekar <akhedekar@qmetric.co.uk>
2013-07-04 11:41:39 +0100

    Fixed the FeedConsumerTest

--- src/test/groovy/com/qmetric/feed/consumer/FeedConsumerTest.groovy
+++ src/test/groovy/com/qmetric/feed/consumer/FeedConsumerTest.groovy
@@ -1,14 +1,9 @@
 package com.qmetric.feed.consumer
 
-import com.theoryinpractise.halbuilder.api.ReadableRepresentation
-import spock.lang.Ignore
 import spock.lang.Specification
 
-@Ignore("Need to refactor FeedConsumer to add tests for this class")
 class FeedConsumerTest extends Specification
 {
-
-    def unconsumedFeedEntriesFinder = Mock(UnconsumedFeedEntriesFinder)
 
     def consumerAction = Mock(ConsumerAction)
 
@@ -16,16 +11,13 @@ class FeedConsumerTest extends Specification
 
     def endpoint = Mock(FeedEndpoint)
 
-    def feedEntry1 = Mock(ReadableRepresentation)
-
-    def feedEntry2 = Mock(ReadableRepresentation)
-
     final consumer = new FeedConsumer(endpoint, consumedFeedEntryStore, consumerAction)
 
     def "should consume all entries returned by finder"()
     {
         given:
-        unconsumedFeedEntriesFinder.findUnconsumed() >> Arrays.asList(feedEntry1, feedEntry2)
+        endpoint.reader() >> new InputStreamReader(this.getClass().getResource('/feedWithAllUnconsumed.json').openStream())
+        consumedFeedEntryStore.notAlreadyConsumed(_) >>> [true, true]
 
         when:
         consumer.consume()
@@ -38,7 +30,8 @@ class FeedConsumerTest extends Specification
     def "should markAsConsumed all entries returned by finder"()
     {
         given:
-        unconsumedFeedEntriesFinder.findUnconsumed() >> Arrays.asList(feedEntry1, feedEntry2)
+        endpoint.reader() >> new InputStreamReader(this.getClass().getResource('/feedWithAllUnconsumed.json').openStream())
+        consumedFeedEntryStore.notAlreadyConsumed(_) >>> [true, true]
 
         when:
         consumer.consume()
