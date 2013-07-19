11aa7750c89f73286293f9dac539b34491516fe4
Huw Lewis <hlewis@qmetric.co.uk>
2013-07-19 12:44:31 +0100

    Added missing accept header to feed request

--- src/main/java/com/qmetric/feed/consumer/RestyFactory.java
+++ src/main/java/com/qmetric/feed/consumer/RestyFactory.java
@@ -2,14 +2,21 @@ package com.qmetric.feed.consumer;
 
 import us.monoid.web.Resty;
 
+import static com.theoryinpractise.halbuilder.api.RepresentationFactory.HAL_JSON;
 import static us.monoid.web.Resty.Option.timeout;
 
 public class RestyFactory
 {
     private static final int CONNECTION_TIMEOUT = 60000;
 
+    private static final String ACCEPT = "Accept";
+
     public static Resty create()
     {
-        return new Resty(timeout(CONNECTION_TIMEOUT));
+        final Resty resty = new Resty(timeout(CONNECTION_TIMEOUT));
+
+        resty.withHeader(ACCEPT, HAL_JSON);
+
+        return resty;
     }
 }
--- src/test/groovy/com/qmetric/feed/consumer/RestyFactoryTest.groovy
+++ src/test/groovy/com/qmetric/feed/consumer/RestyFactoryTest.groovy
@@ -1,7 +1,16 @@
 package com.qmetric.feed.consumer
+
 import spock.lang.Specification
 
+import static com.theoryinpractise.halbuilder.api.RepresentationFactory.HAL_JSON
+
 class RestyFactoryTest extends Specification {
+
+    def "should accept hal+json content"()
+    {
+        expect:
+        RestyFactory.create().getAdditionalHeaders().get("Accept") == HAL_JSON
+    }
 
     def "should create resty with connection timeout"()
     {
